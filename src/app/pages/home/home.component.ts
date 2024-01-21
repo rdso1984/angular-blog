a4e9bd59ff3b6d831aed36422d6111d2641e78b9
rdso1984 <rdso1984@gmail.com>
2024-01-21 02:41:11 -0300

    Start project

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { SmallCardComponent } from '../../components/small-card/small-card.component';
 import { MenuTitleComponent } from '../../components/menu-title/menu-title.component';
 import { BigCardComponent } from '../../components/big-card/big-card.component';
+import { MenuBarComponent } from '../../components/menu-bar/menu-bar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SmallCardComponent, MenuTitleComponent, BigCardComponent],
+  imports: [SmallCardComponent, MenuTitleComponent, BigCardComponent, MenuBarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
